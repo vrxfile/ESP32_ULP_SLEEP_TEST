55d95cfc7e66a2bad220701d1d9950db68c3cd5f
vrxfile <vrxfile@mail.ru>
2017-09-22 13:10:39 +0300

    Made some changes

--- ESP32_ULP_Counter_2.ino
+++ ESP32_ULP_Counter_2.ino
@@ -20,32 +20,28 @@ WiFiClient client;
 // Blynk
 char auth[] = "ab6ca5b76e9c470396f750999f3d3ddc";
 
-#define WAKE_UP_TIME 10   // Wake up time (s)
+#define WAKE_UP_TIME 30   // Wake up time (s)
 #define WORKING_TIME 10   // Working time (s)
 
 long waterCount = 0;     // Суммарные данные со счетчика воды
 long esp_water = 0;      // Данные, посчитанные основным процессором
 long ulp_water = 0;      // Данные, посчитанные ULP сопроцессором
 
-// Вход импульсов (бит 25 в RTC_GPIO_IN_REG)
-int ulp_rtc_io_number = 11;
-gpio_num_t gpio_num = GPIO_NUM_0;
-
 // Код для ULP процессора
 const ulp_insn_t program[] = {
-  I_MOVI(R0, 0),
-  I_MOVI(R1, 0),
-  I_MOVI(R3, 0),
-  M_LABEL(1),
-  I_RD_REG(RTC_GPIO_IN_REG, 25, 25),
-  I_SUBR(R2, R1, R0),
-  M_BXZ(2),
-  I_ADDI(R3, R3, 1),
-  M_LABEL(2),
-  I_MOVR(R1, R0),
-  I_MOVI(R2, 16),
-  I_ST(R3, R2, 0),
-  M_BX(1),
+  I_MOVI(R0, 0),      // Сброс значения регистра R0
+  I_MOVI(R1, 0),      // Сброс значения регистра R1
+  I_MOVI(R3, 0),      // Сброс значения регистра R3
+  M_LABEL(1),         // Метка №1
+  I_RD_REG(RTC_GPIO_IN_REG, 25, 25),  // Чтение состояния ножки входа
+  I_SUBR(R2, R1, R0), // Находим разность между новым состоянием ножки и предыдущим
+  M_BXZ(2),           // Если АЛУ - 0, то переходим в конец цикла, иначе инкрементируем счетчик импульсов
+  I_ADDI(R3, R3, 1),  // Увеличиваем на 1 счетчик импульсов
+  M_LABEL(2),         // Метка №2
+  I_MOVR(R1, R0),     // Сохраняем значение состояния ножки
+  I_MOVI(R2, 16),     // Устанавливаем адрес памяти, куда будем выводить значение счетчика
+  I_ST(R3, R2, 0),    // Вывводим в память RTC значения счетчика
+  M_BX(1),            // Возвращаемся в начало цикла программы
   I_HALT()
 };
 
@@ -72,12 +68,16 @@ void setup() {
     case ESP_DEEP_SLEEP_WAKEUP_TIMER: {
         Serial.println();
         Serial.print("Wake up from timer. Time spent in deep sleep: ");
-        Serial.println(sleep_time_ms);
+        Serial.print(sleep_time_ms);
+        Serial.println(" ms");
+        Serial.println();
         break;
       }
     case ESP_DEEP_SLEEP_WAKEUP_UNDEFINED:
     default: {
+        Serial.println();
         Serial.println("Not a deep sleep reset");
+        Serial.println();
         memset(RTC_SLOW_MEM, 0, CONFIG_ULP_COPROC_RESERVE_MEM);
       }
   }
@@ -89,29 +89,28 @@ void setup() {
     delay(500);
     Serial.print(".");
     }  */
-  //  Blynk.begin(auth, ssid, pass, blynk_ip, 8442);
-  //  Serial.println("");
-  //  Serial.println("WiFi connected");
-  //  Serial.print("IP address: ");
-  //  Serial.println(WiFi.localIP());
-  //  Serial.println();
+  Blynk.begin(auth, ssid, pass, blynk_ip, 8442);
+  Serial.println();
+  Serial.println("WiFi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
 
   // Считывание показаний из ULP сопроцессора
-  ulp_water = RTC_SLOW_MEM[16] & 0xffff;
+  ulp_water = (RTC_SLOW_MEM[16] & 0xffff) / 2;
+  Serial.print("Number of pulses from last sleep: ");
+  Serial.println(ulp_water);
 
   // Отправка данных на сервер Blynk
-  waterCount = ulp_water / 2;
+  waterCount = waterCount + ulp_water;
   Serial.print("Water counter: ");
   Serial.println(waterCount);
-  //  Blynk.virtualWrite(V0, waterCount);
-  //  Blynk.run();
-  //  Blynk.run();
-  //  Blynk.run();
-  //  Serial.println("Data has been sent");
-  //  Serial.println();
-
-  // Задержка перед уходом в спящий режим (только для тестов)
-  //delay(WORKING_TIME * 1000);
+  Blynk.virtualWrite(V0, waterCount);
+  Blynk.run();
+  Blynk.run();
+  Blynk.run();
+  Serial.println("Data has been sent");
+  Serial.println();
 
   // Очистка памяти RTC для ULP сопроцессора
   memset(RTC_SLOW_MEM, 0, CONFIG_ULP_COPROC_RESERVE_MEM);
@@ -121,6 +120,9 @@ void setup() {
   size_t size = sizeof(program) / sizeof(ulp_insn_t);
   ulp_process_macros_and_load(load_addr, program, &size);
   ulp_run(load_addr);
+
+  // Задержка перед уходом в спящий режим (только для тестов)
+  delay(WORKING_TIME * 1000);
 
   // Установка таймера сна
   Serial.print("Enabling timer wakeup: ");
